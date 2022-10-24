bfec436e768b0f1cea342aae961633bf74b8287c
jangsumin <jsm0726@naver.com>
2022-10-24 23:30:11 +0900

    when you click the start button, bird occurs and move responding to arrow key

--- src/index.js
+++ src/index.js
@@ -9,17 +9,55 @@ startBtn.addEventListener("click", start);
 gameMessage.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
+let keys = {};
+let bird = document.createElement("div");
+let wing = document.createElement("div");
+let player = {
+  x: 0,
+  y: 0,
+  speed: 2
+};
 
 function start() {
   console.log("game start");
   startBtn.classList.add("hide");
   gameMessage.classList.add("hide");
+  bird.setAttribute("class", "bird");
+  wing.setAttribute("class", "wing");
+  bird.appendChild(wing);
+  gameArea.appendChild(bird);
+  player.x = bird.offsetLeft;
+  player.y = bird.offsetTop;
+  window.requestAnimationFrame(playGame);
 }
 
-function pressOn() {
-  console.log("on");
+function playGame() {
+  if (keys.ArrowLeft) {
+    player.x -= player.speed;
+  }
+  if (keys.ArrowRight) {
+    player.x += player.speed;
+  }
+  if (keys.ArrowUp) {
+    player.y -= player.speed;
+  }
+  if (keys.ArrowDown) {
+    player.y += player.speed;
+  }
+  bird.style.left = player.x + "px";
+  bird.style.top = player.y + "px";
+  // 재귀적으로 계속 게임을 진행중이도록 함.
+  window.requestAnimationFrame(playGame);
 }
 
-function pressOff() {
-  console.log("off");
+function pressOn(e) {
+  console.log(e.code);
+  keys[e.code] = true;
+  console.log(keys);
+}
+
+function pressOff(e) {
+  console.log(e.code);
+  keys[e.code] = false;
+  console.log(keys);
 }
